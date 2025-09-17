cde9bb41630126e3a4b9f387b61a74237ae82c1d
Srinidhi <write2srinidhi.shankar@gmail.com>
2025-09-17 19:53:12 +0200

    Update Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,23 +11,23 @@ function Skills() {
         <div className="w-full flex flex-col gap-2 text-lg font-normal justify-between mt-12 max-lg:mt-4 max-sm:text-xs max-lg:text-sm max-lg:flex-col">
           <ul className="list-disc list-inside">
             <li>
-              <strong>Mechanical & Mechatronics Engineering:</strong> CAD (Fusion 360, SolidWorks), FEA (ANSYS, SimScale), Mechanical Design, Motion Systems, Sensor Integration, Pneumatics & Actuators.
+              <strong>Mechanical & Mechatronics Engineering:</strong> CAD (Fusion 360, SolidWorks), MATLAB, Motion Systems, Sensor Integration, Pneumatics & Actuators.
             </li>
             <li className="mt-2">
-              <strong>Embedded Systems & Electronics:</strong> Arduino, Raspberry Pi, STM32 (ARM Cortex), BTT Octopus, Atmega328p, PCB Soldering & Debugging, I2C/SPI/UART, Power Supply Design (NDR, SDR series)
+              <strong>Embedded Systems & Electronics:</strong> Arduino, Raspberry Pi, STM32 (ARM Cortex), BTT Octopus, Atmega328p, PCB Soldering & Debugging.
             
             </li>
             <li className="mt-2">
-              <strong>Industrial Automation & Controls:</strong> Siemens SINUMERIK, PROFIBUS/PROFINET, PLCs (Basics), Servo & Stepper Motor Integration (T6 AC Servo, NEMA 23), Endstop & Proximity Sensors, Predictive Maintenance Concepts
+              <strong>Industrial Automation & Controls:</strong> TwinCAT-Software Development, Siemens SINUMERIK, PROFIBUS/PROFINET, Basics of PLC Programming.
             </li>
             <li className="mt-2">
-              <strong>Programming & Scripting:</strong> Python (Data Logging, Automation), MATLAB (Control Systems, Signal Processing)
+              <strong>Programming & Scripting:</strong> Python, C++, Java
             </li>
             <li className="mt-2">
-              <strong>3D Printing & Digital Manufacturing:</strong> FDM/FFF Printing, Bambu Studio, Slicing Optimization, 3D Printer Control Systems, Maintenance & Calibration, Revo Foods Custom Systems
+              <strong>Language Proficiency:</strong> English (Native Speaker C1), German (Working Proficiency A2)
             </li>
             <li className="mt-2">
-              <strong>Data & Analysis Tools:</strong> Excel (Advanced), Zoho Books (Inventory, Marketplace Integration), Root Cause Analysis (RCA), Failure Mode Effects Analysis (FMEA)
+              <strong>Other Softwares:</strong> Microsoft Office Suite (Word, Excel, PowerPoint, Outlook), Zoho Books (Inventory, Marketplace Integration), Bambu Studio Software, Asana (Project management).
             </li>
           </ul>
         </div>
